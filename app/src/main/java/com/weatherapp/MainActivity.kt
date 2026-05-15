d9de8d20434839f21f2b94de5b61de49250feecb
Mariana Santos <mrds.profissional@gmail.com>
2026-05-14 21:11:36 -0300

    Prática 02 - Parte 1

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -12,14 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.weatherapp.ui.theme.WeatherAppTheme
-import android.app.Activity
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-
-
+import com.weatherapp.ui.HomePage
 
 
 class MainActivity : ComponentActivity() {
@@ -54,29 +47,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as Activity
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Text(
-            text = "Bem-vindo à Home!",
-            style = MaterialTheme.typography.headlineMedium
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(onClick = {
-            activity.finish()
-        }) {
-            Text("Sair")
-        }
-    }
-}
