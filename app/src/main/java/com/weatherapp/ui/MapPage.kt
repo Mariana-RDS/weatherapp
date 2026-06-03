ddac70d584c161492b202c03de5f256c9ac89c95
Mariana Santos <mrds.profissional@gmail.com>
2026-06-02 21:11:32 -0300

    Prática 4 - Parte 4

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -1,26 +1,23 @@
 package com.weatherapp.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.weatherapp.model.MainViewModel
-import com.google.maps.android.compose.*
 
 @Composable
 fun MapPage(
@@ -33,6 +30,17 @@ fun MapPage(
 
     val camPosState = rememberCameraPositionState()
 
+    val context = LocalContext.current
+
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
     GoogleMap(
         modifier = modifier.fillMaxSize(),
 
@@ -43,7 +51,15 @@ fun MapPage(
             )
         },
 
-        cameraPositionState = camPosState
+        cameraPositionState = camPosState,
+
+        properties = MapProperties(
+            isMyLocationEnabled = hasLocationPermission
+        ),
+
+        uiSettings = MapUiSettings(
+            myLocationButtonEnabled = true
+        )
     ) {
 
         Marker(
